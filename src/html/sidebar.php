f8c04ad5fa23d7d829350910f4d3f22ee7549c75
Aparna <aparna21nr@gmail.com>
2024-09-29 17:24:06 +0530

    new

--- src/html/sidebar.php
+++ src/html/sidebar.php
@@ -35,6 +35,14 @@
                 <span class="hide-menu">Forms</span>
               </a>
             </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./ui-forms.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-file-description"></i>
+                </span>
+                <span class="hide-menu">Forms</span>
+              </a>
+            </li>
             <li class="nav-small-cap">
               <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
               <span class="hide-menu">UI COMPONENTS</span>
