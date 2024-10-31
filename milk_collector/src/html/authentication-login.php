7ffc59751cecf2434da024f89a5164c9bdb86437
Aparna <aparna21nr@gmail.com>
2024-10-31 21:11:34 +0530

    modified milk collector signin

--- milk_collector/src/html/authentication-login.php
+++ milk_collector/src/html/authentication-login.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
+  <title>Milk Collector</title>
   <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
@@ -20,7 +20,7 @@
           <div class="col-md-8 col-lg-6 col-xxl-3">
             <div class="card mb-0">
               <div class="card-body">
-                <a href="./index.php" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                <a href="" class="text-nowrap logo-img text-center d-block py-3 w-100">
                   <img src="../assets/images/logos/dark-logo.svg" width="180" alt="">
                 </a>
                 <p class="text-center">Your Social Campaigns</p>
@@ -42,8 +42,8 @@
     if (isset($_GET['error'])) {
         echo "<div  class='flex-container' style='text-align:center;color:red;'>Invalid username or password!!</div>";
     }
-    if (isset($_GET['yes'])) {
-      echo "<div  class='flex-container' style='text-align:center;color:red;'>You are yet to be authenticated by your society !!<br> Contact your respective society for further details!!</div>";
+    if (isset($_GET['status'])) {
+      echo "<div  class='flex-container' style='text-align:justify;color:red;'>You are yet to be authenticated by your society !! Contact your respective society for further details!!</div>";
   }
     ?>
    <br><br>               <div class="d-flex align-items-center justify-content-center">
--- milk_collector/src/html/authentication-register.php
+++ milk_collector/src/html/authentication-register.php
@@ -7,7 +7,7 @@ include "../databases/connection.php";
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
+  <title>Milk Collector</title>
   <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="../assets/css/styles.min.css" />
 </head>
@@ -92,7 +92,7 @@ include "../databases/connection.php";
                  
                   <div class="d-flex align-items-center justify-content-center">
                     <p class="fs-4 mb-0 fw-bold">Already have an Account?</p>
-                    <a class="text-primary fw-bold ms-2" href="./authentication-register.php">Sign In</a>
+                    <a class="text-primary fw-bold ms-2" href="./authentication-login.php">Sign In</a>
                   </div>
                 </form>
               </div>
